142c521c77ec63634479639abdd525542bd16f90
lars-tps <49183778+lars-tps@users.noreply.github.com>
2019-04-26 02:01:51 +0800

    Cloned to desktop

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is my repo for the first project
+Hey there! This is a number guessing game written with python. 
--- guessing_game.py
+++ guessing_game.py
@@ -11,39 +11,65 @@ NOTE: If you strongly prefer to work locally on your own computer, you can total
 
 import random
 
+def input_prompt(text):
+    while True:
+        user_input = input(f"{text}")
+        if user_input.isdigit() != True:
+            print("**Error**\nThat's not an integer or it's not a positive number!\n")
+            continue
+        elif int(user_input) < 1 or int(user_input) > 10:
+            print("**Error**\nThat number is out of range!\n")
+            continue
+        elif user_input.isdigit() == True and (int(user_input) >= 1 and int(user_input) <= 10):
+            break
+    user_input = int(user_input)
+    return(user_input)
+
+def yes_or_no(x):
+    x_input = str(input(x))
+    while x_input.lower() != "y" and x_input.lower() != "n":
+        print("**Error!**\nPlease only enter 'Y' or 'N'\n")
+        x_input = str(input(x))
+    return(x_input)
 
 def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
     print(f"""
-    {"-"*10}
+    {"-"*30}
     Welcome to the number guessing game!
-    {"-"*10}
+    {"-"*30}
     """)
-    answer = random.randint(0,10)
-    user_guess = int(input("What's your guess?"  ))
-    while user_guess != answer:
-        if user_guess < answer:
-            user_guess = int(input("It's higher!"))
-        if user_guess > answer:
-            user_guess = int(input("It's lower!"))
-    print("You got it! Thanks for playing.")
-
+    
+    attempt_history = []
+    
+    while True:
+        answer = random.randint(1,10)
+        user_guess = input_prompt("Pick a number between 1 and 10.\n> ")
+        answer_list = []
+        answer_list.append(user_guess)
+    
+        while user_guess != answer:
+            if user_guess < answer:
+                user_guess = input_prompt("It's higher!\n> ")
+                answer_list.append(user_guess)
+            if user_guess > answer:
+                user_guess = input_prompt("It's lower!\n> ")
+                answer_list.append(user_guess)
+        
+        attempts = len(answer_list)
+        attempt_history.append(attempts)
+        
+        print(f"You got it! The answer is '{answer}'. You made {attempts} attempts. Thanks for playing.")
+        
+        play_again = yes_or_no("Would you like to play again? (Y/N):  ")
+        
+        if play_again == 'y':
+            print(f"The current high score is: {min(attempt_history)} attempts")
+            continue
+        elif play_again == 'n':
+            if attempt_history != 0:
+                print(f"The best score is: {min(attempt_history)} attempts")
+            print("Thanks again for playing the game!")
+            break
 
 
 if __name__ == '__main__':
